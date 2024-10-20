de47df428e04fbe58fd00c9feb6923b86689fc99
Robin Stumpf <rstumpf@1-C-26.42heilbronn.de>
2024-10-20 15:10:03 +0200

    added hexadecimal function

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,12 @@
 /*   By: rstumpf <rstumpf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 08:13:30 by rstumpf           #+#    #+#             */
-/*   Updated: 2024/10/20 13:05:41 by rstumpf          ###   ########.fr       */
+/*   Updated: 2024/10/20 15:02:25 by rstumpf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
-# include <stdarg.h>
+#include "ft_printf.h"
 
 // static	int	count_format_specifiers(char *format)
 // {
@@ -23,6 +23,26 @@
 // 			count ++;
 // 	return (count);
 // }
+
+static	int	print_hexadecimal(char lower_or_upper, unsigned int x)
+{
+	int		count;
+	char	*hexadecimal;
+	int		charswritten;
+
+	if (lower_or_upper == 'x')
+		hexadecimal = "0123456789abcdef";
+	else
+		hexadecimal = "0123456789ABCDEF";
+	if (x < 16)
+		return (ft_putchar_fd(hexadecimal[x], 1));
+	else
+	{
+		count = print_hexadecimal(lower_or_upper, x / 16);
+		count += print_hexadecimal(lower_or_upper, x % 16);
+		return (count);
+	}
+}
 
 static	int	print_different_formats(char *s, va_list ap)
 {
@@ -36,6 +56,14 @@ static	int	print_different_formats(char *s, va_list ap)
 		return (ft_putnbr_fd(va_arg(ap, int), 1));
 	else if (*s == 'i')
 		return (ft_putnbr_fd(va_arg(ap, int), 1));
+	else if (*s == 'u')
+		return (ft_putunsignednbr_fd(va_arg(ap, int), 1));
+	else if (*s == 'x')
+		return (print_hexadecimal('x', va_arg(ap, unsigned int)));
+	else if (*s == 'X')
+		return (print_hexadecimal('X', va_arg(ap, unsigned int)));
+	// else if (*s == 'p')
+	// 	return (print_hexadecimal(va_arg(ap, unsigned int)));
 	return (1);
 }
 
@@ -57,13 +85,20 @@ int	ft_printf(char *format, ...)
 		count_output += ft_putchar_fd(*format, 1);
 		format++;
 	}
+	va_end(ap);
 	return (count_output);
 }
 
 int	main(void)
 {
 	int	count;
+	int count2;
+	int hexa = -12;
+	count2 = 20;
 
-	count = ft_printf("Hello %s\n", "John");
-	ft_printf("The chars written are %d\n", count);
+
+	count = ft_printf("The chars written are, in hexadecima %d, %x\n", count2, hexa);
+	ft_printf("%d\n", count);
+	count = printf("The chars written are, in hexadecima %d, %x\n", count2, hexa);
+	printf("%d\n", count);
 }
